f2b1640d893a7d62bacffc8f6f80c2713fd20143
Isaac <17958168+iaeshliman@users.noreply.github.com>
2018-11-20 22:16:01 -0600

    Improved the buildCalendar
    The buildCalendar.php now actually builds the tutoring page that the student will view. I included some of the preexisting content (head, footer, stlye, etc...) and now it looks like a moderatly acceptable schedule. Will need modification to style and restyle of admin page after calendar creation.

--- current_students/tutoring/admin/buildCalendar.php
+++ current_students/tutoring/admin/buildCalendar.php
@@ -1,4 +1,5 @@
 <?php
+///////////////////////// Reads the CSV and makes the table /////////////
 	// Makes the array for all events
 	$events;
 	
@@ -18,6 +19,9 @@
 		$array = explode(";",$line);
 		$events[$array[0]][] = $array;
 	}
+	
+	// Closes the file
+	fclose($csvFile);
 	
 	// Creates an array of the keys for the events
 	$eventsKeys = array_keys($events);
@@ -56,31 +60,31 @@
 				// Creates a div element with the start and end time displayed for each event
 				case "Sunday":
 					$sunString .= "<div>Start Time: " . $events[$eventsKeys[$i]][$ii][1]
-						. "<br>End Time: " . $events[$eventsKeys[$i]][$ii][2] . "</div>";
+						. "<br>End Time: " . $events[$eventsKeys[$i]][$ii][2] . "</div><hr>";
 					break;
 				case "Monday":
 					$monString .= "<div>Start Time: " . $events[$eventsKeys[$i]][$ii][1]
-						. "<br>End Time: " . $events[$eventsKeys[$i]][$ii][2] . "</div>";					
+						. "<br>End Time: " . $events[$eventsKeys[$i]][$ii][2] . "</div><hr>";					
 					break;
 				case "Tuesday":
 					$tueString .= "<div>Start Time: " . $events[$eventsKeys[$i]][$ii][1]
-						. "<br>End Time: " . $events[$eventsKeys[$i]][$ii][2] . "</div>";
+						. "<br>End Time: " . $events[$eventsKeys[$i]][$ii][2] . "</div><hr>";
 					break;
 				case "Wednesday":
 					$wedString .= "<div>Start Time: " . $events[$eventsKeys[$i]][$ii][1]
-						. "<br>End Time: " . $events[$eventsKeys[$i]][$ii][2] . "</div>";
+						. "<br>End Time: " . $events[$eventsKeys[$i]][$ii][2] . "</div><hr>";
 					break;
 				case "Thursday":
 					$thuString .= "<div>Start Time: " . $events[$eventsKeys[$i]][$ii][1]
-						. "<br>End Time: " . $events[$eventsKeys[$i]][$ii][2] . "</div>";
+						. "<br>End Time: " . $events[$eventsKeys[$i]][$ii][2] . "</div><hr>";
 					break;
 				case "Friday":
 					$friString .= "<div>Start Time: " . $events[$eventsKeys[$i]][$ii][1]
-						. "<br>End Time: " . $events[$eventsKeys[$i]][$ii][2] . "</div>";
+						. "<br>End Time: " . $events[$eventsKeys[$i]][$ii][2] . "</div><hr>";
 					break;
 				case "Saturday":
 					$satString .= "<div>Start Time: " . $events[$eventsKeys[$i]][$ii][1]
-						. "<br>End Time: " . $events[$eventsKeys[$i]][$ii][2];
+						. "<br>End Time: " . $events[$eventsKeys[$i]][$ii][2] . "</div><hr>";
 					break;
 			}
 		}
@@ -101,10 +105,35 @@
 	
 	// Displays the calendar
 	
-	echo $calendar;
+	echo "Preformating and style<hr><br>" . $calendar;
 	
+////////////////////// Creates the PHP page string /////////////////
 	
+	// Creates a string to write to file
+	$tutoringString = "";
 	
+	// Adds the includes and the calendar
+	$tutoringString .= "<?php $" . "path = '../../../'; ?>";
+	$tutoringString .= "<?php include $" . "path . 'head1.html'; ?>";
+	$tutoringString .= "<?php include $" . "path . 'head2.php'; ?>";
+	$tutoringString .= $calendar;
+	$tutoringString .= "<?php include $" . "path . 'footer.php'; ?>";
+	$tutoringString .= "</body></html>";
 	
+////////////////////// Writes the PHP page string to a PHP file ///////////
+
+	// Creates and opens the file "tutoring.php"
+	$tutoringFileName = "tutoring.php";
+	$tutoringFile = fopen($tutoringFileName,"w");
+	if(!$tutoringFile)
+	{
+		die("Unable to write to file $tutoringFileName");
+	}
 	
+	// Writes the tutoringString to the file making the PHP page
+	fwrite($tutoringFile,$tutoringString);
+	// Closes the tutoringFile
+	fclose($tutoringFile);
 ?>
+
+<a href='tutoring.php'>Tutoring Schedule</a>
