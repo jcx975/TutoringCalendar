5e8316f57ca596edd3a1c232b49b2895b21af770
Justin <36967116+jcx975@users.noreply.github.com>
2018-12-07 18:40:18 -0600

    Move student page

--- current_students/tutoring/index.php
+++ current_students/tutoring/index.php
@@ -1,12 +1,11 @@
 <?php
-    $path = "../../../";
+    $path = "../../";
     include $path."head1.html";
 ?>
 	<!-- <link rel="stylesheet" type="text/css" href="../css/tableStyles.css"> -->
-	<link rel="stylesheet" type="text/css" href="../css/index.css">
+	<link rel="stylesheet" type="text/css" href="./css/index.css">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script src="../js/admin.js"></script>
-    <script src="../js/classFilter.js"></script>
+    <script src="./js/classFilter.js"></script>
 <?php
     include $path."head2.php";
 ?>
@@ -28,7 +27,7 @@ Filter by class:
 	<div class="row schedule purple-background round full-width">
 		<h2 class="white">Schedule</h2>
 <?php
-	include "schedule.html";
+	include "./admin/schedule.html";
 ?>
 	</div>
 
